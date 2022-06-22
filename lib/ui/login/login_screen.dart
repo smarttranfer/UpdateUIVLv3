32528a28e5d912474ec7e59f52d37e42a1fe3292
smarttranfer@gmail.com <smarttranfer@gmail.com>
2022-06-23 00:54:38 +0700

    update ui doi domain
    update ui doi domain

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -18,14 +18,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool _rememberMe = false;
   final TextEditingController username = TextEditingController();
   final TextEditingController password = TextEditingController();
-  void saveuser(String user , String password)async{
+  final TextEditingController domain = TextEditingController();
+  bool _visible = false;
+  void saveuser(String user, String password) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString("username", user);
     await prefs.setString("password", password);
   }
+
   Widget _buildEmailTF() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -89,6 +91,41 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
+  Widget _buildDomainTF() {
+    return Visibility(
+        visible: _visible,
+        child: AnimatedOpacity(
+            opacity: _visible ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 1000),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  decoration: kBoxDecorationStyle,
+                  height: 60.0,
+                  child: TextField(
+                    controller: domain,
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'OpenSans',
+                    ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(top: 14.0),
+                      prefixIcon: Icon(
+                        Icons.domain,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Enter Domain',
+                      hintStyle: kHintTextStyle,
+                    ),
+                  ),
+                ),
+              ],
+            )));
+  }
 
   Widget _buildLoginBtn() {
     return Container(
@@ -96,18 +133,24 @@ class _LoginScreenState extends State<LoginScreen> {
       width: double.infinity,
       child: RaisedButton(
         elevation: 5.0,
-        onPressed: () async{
+        onPressed: () async {
           final prefs = await SharedPreferences.getInstance();
           await fn_login.fn_loginapp(username.text, password.text);
           String token = prefs.getString("token").toString();
           await fn_DataCustomer.getDataCustomer(token);
-          if(login.LoginSucces==true){
-            saveuser(username.text,password.text);
-            Navigator.pushReplacement(context, PageTransition(type: PageTransitionType.rightToLeft,child: Home_page()));
-          }else{
+          if (login.LoginSucces == true) {
+            saveuser(username.text, password.text);
+            if(domain.text.isNotEmpty && _visible==true){
+              await prefs.setString("domain", domain.text);
+            }
+            Navigator.pushReplacement(
+                context,
+                PageTransition(
+                    type: PageTransitionType.rightToLeft, child: Home_page()));
+          } else {
             _showErrorMessage(login.dataError);
           }
-        } ,
+        },
         padding: EdgeInsets.all(15.0),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
@@ -148,8 +191,16 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Image.asset('assets/ic_app/icon_laucher.png',height: 200,width: 200,),
-                      SizedBox(height: 30.0),
+
+
+                      Image.asset(
+                        'assets/ic_app/icon_laucher.png',
+                        height: 200,
+                        width: 200,
+                      ),
+                      // SizedBox(height: 30.0),
+                      _buildDomainTF(),
+                      SizedBox(height: 20.0),
                       _buildEmailTF(),
                       SizedBox(
                         height: 30.0,
@@ -159,6 +210,26 @@ class _LoginScreenState extends State<LoginScreen> {
                         height: 10.0,
                       ),
                       _buildLoginBtn(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Đổi Doamin',
+                            style: kHintTextStyle,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.swap_horiz_outlined,
+                              color: _visible ? Colors.green : Colors.white54,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _visible = !_visible;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
@@ -168,25 +239,27 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
-
   }
+
   _showErrorMessage(String e) {
     showCupertinoDialog(
         context: context,
         builder: (context) => Theme(
-          data: ThemeData.dark(),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-            child: CupertinoAlertDialog(
-                title: Text(
-                    "Warning"),
-                content: Text(e),
-                actions: [
-                  CupertinoDialogAction(
-                      child: Text("Close",style: TextStyle(color: App_Color.green),),
-                      onPressed: () => Navigator.pop(context))
-                ]),
-          ),
-        ));
+              data: ThemeData.dark(),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+                child: CupertinoAlertDialog(
+                    title: Text("Warning"),
+                    content: Text(e),
+                    actions: [
+                      CupertinoDialogAction(
+                          child: Text(
+                            "Close",
+                            style: TextStyle(color: App_Color.green),
+                          ),
+                          onPressed: () => Navigator.pop(context))
+                    ]),
+              ),
+            ));
   }
 }
